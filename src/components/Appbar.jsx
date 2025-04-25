7c375c9d0d8bf462e8798d8b72571e0fe287729d
AYUSH SINGH <ayushsinghpanchet07@gmail.com>
2025-04-25 19:15:20 +0530

    changes the name

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -26,7 +26,7 @@ export const Appbar = () => {
     return (
         <div className="shadow h-14 flex justify-between">
             <div className="flex flex-col justify-center h-full ml-4 text-2xl font-bold text-black">
-                PayMate
+                PayNet
             </div>
             <div className="flex items-center">
                 <div className="flex flex-col justify-center h-full ml-4 mr-2 font-semibold">
